7111119d5de06aa55b2ff79b8513fd25592bc5e0
hmbashar <hmbashar@gmail.com>
2023-03-30 17:13:32 +0600

    cart quantity and item update with advance way

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,19 +5,23 @@ const Cart = ({cart}) => {
 
     let total = 0;
     let totalShipping = 0 ;
+    let quantity = 0;
     for (const product of cart) {
-        total = total + product.price; // calculation total price for products in cart
+        product.quantity = product.quantity || 1 ;
+        total = total + product.price * product.quantity; // calculation total price for products in cart
         totalShipping = totalShipping + product.shipping; // calculation total Shipping cost for products in cart
+
+        quantity = quantity + product.quantity;
     }
 
     const tax = total*7/100;
 
     const grandTOtal = total + totalShipping + tax ;
-    console.log(cart);
+    
     return (
         <div className='cart'>
              <h4>Order Summery</h4>
-                <p>Selected Item: {cart.length}</p>
+                <p>Selected Item: {quantity}</p>
                 <p>Total Price: ${total}</p>
                 <p>Total Shipping: ${totalShipping} </p>
                 <p>Tax: ${tax.toFixed(2)}</p>
