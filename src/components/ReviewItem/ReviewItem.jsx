993c5e37c185eab76a62d12a18972c39aa404852
hmbashar <hmbashar@gmail.com>
2023-04-23 02:07:02 +0600

    review item design

---
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './ReviewItem.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+
+
+const ReviewItem = ({ product }) => {
+    const { id, img, price, name, quantity } = product;
+    return (
+        <div className='review-item'>
+            <img src={img} alt="" />
+            <div className="review-details">
+                <p className='product-title'>{name}</p>
+                <p className='price'>Price: <span className='review-price'>${price}</span></p>
+                <p className='quantity'>Quantity: <span className='review-price'>{quantity}</span></p>
+            </div>
+            <button className='btn-delete'>
+                <FontAwesomeIcon icon={faTrashAlt} />
+            </button>
+        </div>
+    );
+};
+
+export default ReviewItem;
